71c8d91649580a664d6b3871a051e745244121d2
marcosdarri <marcosdarri@hotmail.com>
2024-07-18 15:45:46 -0300

    Feat: Adds Portfolio page.

---
+++ app/components/Portfolio.jsx
@@ -0,0 +1,14 @@
+import PortfolioPage from "./PortfolioPage";
+
+const Portfolio = () => {
+  return (
+    <div className="hero bg-base-200 min-h-screen" id="portfolio">
+      <PortfolioPage />
+      <PortfolioPage />
+      <PortfolioPage />
+      <PortfolioPage />
+    </div>
+  );
+};
+
+export default Portfolio;
--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Footer from "./components/Footer";
+import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
     <main>
       <Header />
       <Hero />
+      <Portfolio />
       <Contact />
       <Footer />
     </main>
