9b734a80e77b9ee048570519c5da1646bfe94bca
marcosdarri <marcosdarri@hotmail.com>
2024-07-19 16:59:50 -0300

    Feat: Adds About section component.

--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,15 @@
-import Header from "./components/Header";
 import Hero from "./components/Hero";
-import Footer from "./components/Footer";
 import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
+import About from "./components/About";
 
 export default function Home() {
   return (
     <main>
-      <Header />
       <Hero />
+      <About />
       <Portfolio />
       <Contact />
-      <Footer />
     </main>
   );
 }
